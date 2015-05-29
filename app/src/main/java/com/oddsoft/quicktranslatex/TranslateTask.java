0feede626baec417f8163e6ecff1c83d60ff6307
Andy Cheng <ccjeng@gmail.com>
2015-05-29 14:01:41 +0800

    change service provider to Microsoft Translate API

--- app/src/main/java/com/oddsoft/quicktranslatex/TranslateTask.java
+++ app/src/main/java/com/oddsoft/quicktranslatex/TranslateTask.java
@@ -1,18 +1,35 @@
 package com.oddsoft.quicktranslatex;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
+import java.io.StringReader;
+import java.io.Writer;
+import java.io.StringWriter;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
 
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+
+import org.xml.sax.InputSource;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class TranslateTask implements Runnable {
 	private static final String TAG = "TranslateTask";
@@ -55,41 +72,96 @@ public class TranslateTask implements Runnable {
 		String result = translate.getResources().getString(R.string.translation_error);
 		Log.d(TAG, "doTranslate(" + original + ", " + from + ", "	+ to + ")");
 
+		// Construct content
+		String content = "grant_type=client_credentials";
+		content += "&client_id=android-oddsoft-quicktranslatepro";
+		content += "&client_secret=" + URLEncoder.encode("gHaxgCWnUzhAtUuVXR5MKOJ2v+5cCAp7VlmmUN/NeFQ=");
+		content += "&scope=http://api.microsofttranslator.com";
+
+		String q = original;
+
 		try {
 			// Check if task has been interrupted
 			if (Thread.interrupted())
 				throw new InterruptedException();
 
-			String q = URLEncoder.encode(original, "UTF-8");
+			// Send data
+			URL url = new URL("https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/");
+			URLConnection conn = url.openConnection();
 
-            //http://frengly.com?src=fr&dest=en&text=Bonjour+monsieur&email=ccjeng@gmail.com&password=ab1234&outformat=json
-            String url = "http://frengly.com?src="+from+"&dest="+to+"&text="+q+"&email=ccjeng@gmail.com&password=ab1234&outformat=json";
+			// Let the run-time system (RTS) know that we want input.
+			conn.setDoInput(true);
+			// Let the RTS know that we want to do output.
+			conn.setDoOutput(true);
+			// No caching, we want the real thing.
+			conn.setUseCaches(false);
+			// Specify the content type.
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-            Log.d(TAG, url);
+			// Send POST output.
+			DataOutputStream printout = new DataOutputStream(conn.getOutputStream());
+			printout.writeBytes(content);
+			printout.flush();
+			printout.close();
 
-            InputStream is= new URL(url).openStream();
-            try {
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			// Get response data.
+			DataInputStream input = new DataInputStream(conn.getInputStream());
+			String str = "";
+			int start = 0;
+			int end = 0;
+			String accessToken = "";
 
-                String jsonText = readAll(rd);
-                JSONObject json = new JSONObject(jsonText);
 
-                //Log.d(TAG, "json = " + json);
-                result = json.get("translation").toString();
+			while (null != ((str = input.readLine()))) {
+				Log.d(TAG, "access token: " + str);
 
-            } finally { // http://java.sun.com/j2se/1.5.0/docs/guide/net/http-keepalive.html
-                is.close();
-            }
+				JSONObject json = new JSONObject(str);
+				accessToken = json.get("access_token").toString();
 
-			// Check if task has been interrupted
-			if (Thread.interrupted())
-				throw new InterruptedException();
-	     } catch (IOException e) {
+				Log.d(TAG, "access token: " + accessToken);
+			}
+			input.close();
+
+			//Call Microsoft Translate
+			HttpClient httpclient = new DefaultHttpClient();
+			String urlStr = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text="
+					+ URLEncoder.encode(q)
+					+ "&from=" + from
+					+ "&to=" + to;
+			urlStr = urlStr + "&appId=" + URLEncoder.encode("Bearer "+accessToken);
+			HttpGet httpGet = new HttpGet(urlStr);
+			HttpResponse response = httpclient.execute(httpGet);
+			Log.d(TAG, "Response = " + response.getStatusLine().toString());
+			HttpEntity entity = response.getEntity();
+
+			if (response != null) {
+				InputStream instream = entity.getContent();
+				int l;
+				Writer writer = new StringWriter();
+				char[] buffer = new char[1024];
+				Reader reader = new BufferedReader(new InputStreamReader(instream, "UTF-8"));
+				while ((l = reader.read(buffer)) != -1) {
+					writer.write(buffer, 0, l);
+				}
+
+				Log.d(TAG, writer.toString());
+
+				//print result
+				DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+				Document doc = db.parse(new InputSource(new StringReader(writer.toString())));
+				result = doc.getFirstChild().getTextContent();
+
+			}
+
+	     } catch (Exception e) {
 				Log.e(TAG, "IOException", e);
-	     } catch (InterruptedException e) {
+			result = translate.getResources().getString(
+					R.string.translation_interrupted);
+	   /*  } catch (InterruptedException e) {
 			Log.d(TAG, "InterruptedException", e);
 				result = translate.getResources().getString(
 						R.string.translation_interrupted);
+						*/
 	     } finally {
 	    	 //
 			}
@@ -99,15 +171,4 @@ public class TranslateTask implements Runnable {
 		return result;
 	}
 
-    private static String readAll(BufferedReader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-
-        String line = null;
-
-        while ((line = rd.readLine()) != null) {
-            sb.append(line + "\n");
-        }
-
-        return sb.toString();
-    }
 }
