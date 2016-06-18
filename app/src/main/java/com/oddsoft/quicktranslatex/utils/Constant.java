918d5adb117d51c64c090c10efe8d45897d189fc
andycheng <ccjeng@gmail.com>
2016-06-18 22:03:26 +0800

    no message

---
+++ app/src/main/java/com/oddsoft/quicktranslatex/utils/Constant.java
@@ -0,0 +1,20 @@
+package com.oddsoft.quicktranslatex.utils;
+
+import com.oddsoft.quicktranslatex.BuildConfig;
+
+/**
+ * Created by andycheng on 2016/1/31.
+ */
+public class Constant {
+
+    //GA
+    public static final String PROPERTY_ID = "UA-19743390-11";
+
+    //Firebase
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_URL;
+
+    //Admob
+    public static final String ADMOB_TEST_DEVICE = "3C8B080F4F918474804E2685BA46E2B3";
+    public static final String ADMOB_QT_HISTORY = BuildConfig.ADMOB_QT_HISTORY;
+
+}
