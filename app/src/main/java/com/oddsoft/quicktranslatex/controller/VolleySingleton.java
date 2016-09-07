124dc7f54c852b1173d93ab78afa56a601966aab
andycheng <ccjeng@gmail.com>
2016-09-07 12:43:13 +0800

    Double CheckLock Singletion

--- app/src/main/java/com/oddsoft/quicktranslatex/controller/VolleySingleton.java
+++ app/src/main/java/com/oddsoft/quicktranslatex/controller/VolleySingleton.java
@@ -1,12 +1,9 @@
 package com.oddsoft.quicktranslatex.controller;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /**
@@ -14,37 +11,28 @@ import com.android.volley.toolbox.Volley;
  */
 public class VolleySingleton {
 
-    private static VolleySingleton mInstance;
+    private static VolleySingleton mInstance = null;
     private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
     private static Context mCtx;
 
     private VolleySingleton(Context context) {
         mCtx = context;
         mRequestQueue = getRequestQueue();
-
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
     }
 
-    public static synchronized VolleySingleton getInstance(Context context) {
+    public static VolleySingleton getInstance(Context context) {
+
+        /* Double CheckLock Singletion (DCL)
+        **/
         if (mInstance == null) {
-            mInstance = new VolleySingleton(context);
+            synchronized (VolleySingleton.class) {
+                if (mInstance == null) {
+                    mInstance = new VolleySingleton(context);
+                }
+            }
         }
         return mInstance;
+
     }
 
     public RequestQueue getRequestQueue() {
@@ -60,7 +48,5 @@ public class VolleySingleton {
         getRequestQueue().add(req);
     }
 
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
-    }
+
 }
